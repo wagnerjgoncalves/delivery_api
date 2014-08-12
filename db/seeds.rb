e3f894d687f13260176a01f9cf8048f10f96b6ae
Wagner J Goncalves <wagnerjgoncalves@gmail.com>
2014-08-12 19:01:23 -0300

    Add a default data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+MapRoute.destroy_all
+Map.destroy_all
+
+map = Map.create!(name: "SP")
+MapRoute.create!(from: "A", to: "B", distance: 10, map_id: map.id)
+MapRoute.create!(from: "B", to: "D", distance: 15, map_id: map.id)
+MapRoute.create!(from: "A", to: "C", distance: 20, map_id: map.id)
+MapRoute.create!(from: "C", to: "D", distance: 30, map_id: map.id)
+MapRoute.create!(from: "B", to: "E", distance: 50, map_id: map.id)
+MapRoute.create!(from: "D", to: "E", distance: 30, map_id: map.id)
